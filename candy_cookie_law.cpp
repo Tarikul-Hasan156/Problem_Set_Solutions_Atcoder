d0914d21bdb0a12261d6fc635ea00d06c9a79d0d
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-11-02 17:51:42 +0600

    Add files via upload

---
+++ candy_cookie_law.cpp
@@ -0,0 +1,25 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+     
+void solve ()
+{
+    ll a,b,c,d;cin>>a>>b>>c>>d;
+     if (c>=a and d<b)cout<<"Yes"<<endl;
+    else cout<< "No"<<endl;
+}
+int main ()
+{
+    speed;
+    // int tt;
+    // cin>>tt;
+    // while (tt--)
+    // {
+        solve ();
+    // }
+    return 0;
+}
