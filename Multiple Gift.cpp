60438e860d989caff26c3800197b69578b3dd489
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-11-09 00:09:36 +0600

    Add files via upload

---
+++ Multiple Gift.cpp
@@ -0,0 +1,29 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+     
+void solve ()
+{
+    ll x,y;cin>>x>>y;
+    ll res=0;
+    while (x<=y){
+        x+=x;
+        res++;
+    }
+    cout<<res<<endl;
+}
+int main ()
+{
+    speed;
+    // int tt;
+    // cin>>tt;
+    // while (tt--)
+    // {
+        solve ();
+    // }
+    return 0;
+}
